dd17e7f40f7daafc2923d9e54637b8ac4719675c
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-11 15:18:34 +0200

    snack_2

---
+++ snack_2/main.js
@@ -0,0 +1,24 @@
+// A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+// creo un'array in minuscolo
+const anime = ["bleach", "NARUTO", "gintama"]
+
+
+// creo un altro array con map
+const capitalizedAnime = anime.map((element) =>{
+    //faccio una variabile dove trasformo le lettere maiuscole in minuscolo
+    const lower = element.toLowerCase()
+
+    // seleziono la prima lettera dell'array per trasformarla in maiuscolo
+    firstLetterAnime = element.charAt(0).toUpperCase()
+    console.log(firstLetterAnime)
+
+    // costante remain per selezionare il resto della parola in minuscolo
+    const remain = lower.slice(1)
+    
+    // combino le due variabili per formare la parola completa
+     return  combined = firstLetterAnime + remain
+    
+})
+
+console.log(capitalizedAnime)

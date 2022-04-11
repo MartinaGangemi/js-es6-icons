c7c0a39a23cdfea7cb33d081fbaf0d37a5a4dfbc
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-11 15:42:43 +0200

    snack_4

---
+++ snack_4/main.js
@@ -0,0 +1,18 @@
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+
+const persone = [
+    {nome: "Anna", cognome:"Pasquale", età: 18},
+    {nome: "Mario", cognome:"Rossi", età: 33},
+    {nome: "Isa", cognome:"Pinta", età: 16},
+    {nome: "Luigi", cognome:"Fausto", età: 14},
+    {nome: "Alex", cognome:"Cannuccia", età: 28},
+]
+
+const users = persone.map((persona) => {
+    if (persona.età<18){
+        console.log(`${persona.nome} che ha ${persona.età} anni può guidare`)
+    } else {
+        console.log(`${persona.nome} che ha ${persona.età} anni non può guidare`)
+    }
+})
